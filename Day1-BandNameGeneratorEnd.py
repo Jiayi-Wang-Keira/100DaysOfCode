22159a0e0dab00942beedfcb04731f30a7e39cae
Jiayi-Wang-Keira <104404877+Jiayi-Wang-Keira@users.noreply.github.com>
2022-07-26 09:31:48 +0800

    Add files via upload

---
+++ Day1-BandNameGeneratorEnd.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator.')
+CityName=input("What's name of the city you grew up in?\n")
+PetName=input("What's your pet's name?\n")
+print(f"Your band name could be {CityName} {PetName}")
