f36edfa1c34fd0c0250541761ef917212a6fe833
Jiayi-Wang-Kya <104404877+Jiayi-Wang-Kya@users.noreply.github.com>
2022-08-26 14:32:40 +0800

    Add files via upload

--- Day20&21-SnakeGame/score.py
+++ Day20&21-SnakeGame/score.py
@@ -9,13 +9,23 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
-        self.write(f"Score: {self.score}", align="center", font=("Calibri", 20, "normal"))
+        with open("data.txt") as data:
+            self.highest_score = int(data.read())
+        self.update_score_board()
+
+    def update_score_board(self):
+        self.write(f"Score: {self.score} Highest Score: {self.highest_score}", align="center", font=("Calibri", 20, "normal"))
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Calibri", 20, "normal"))
+        self.update_score_board()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game Over", align="center", font=("Calibri", 20, "normal"))
+    def reset(self):
+        if self.score > self.highest_score:
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.score))
+            self.highest_score = self.score
+        self.score = 0
+        self.clear()
+        self.update_score_board()
